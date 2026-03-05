f829afc438be8ad01d5143dd07441111374790d2
Your Name <you@example.com>
2026-03-05 13:39:37 -0500

    feat(flake.nix): add Nix flake for OCaml development environment

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Budget Backend OCaml Development Environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = (with pkgs.ocamlPackages; [
+            ocaml
+            dune_3
+            findlib
+            dream
+            yojson
+            cohttp-lwt
+            cohttp-lwt-unix
+            lwt_ppx
+            ocaml-lsp
+            caqti
+            caqti-lwt
+            caqti-driver-sqlite3
+          ]) ++ [ 
+            pkgs.sops
+            pkgs.sqlite 
+          ];
+        };
+      }
+    );
+}
